aee1dc9956391d938974199c65821fd8b4019ad7
DaBu Games <buitelaardaan2004@gmail.com>
2025-12-04 13:01:27 +0100

    zoom in / out
    added a zoom in / out to the camera move ment script. I also added a bounds to the camera so you can't move the canva's out of the screen.

--- scripts/ProjectSettings.cs
+++ scripts/ProjectSettings.cs
@@ -5,10 +5,17 @@ public partial class ProjectSettings : Node
 {
     public static ProjectSettings Instance { get; private set; }
     
+    // draw
     [Export]public int BrushWidth { get; private set; }
     [Export]public int EraseWidth { get; private set; }
     [Export]public Color BurshColor { get; private set; }
     [Export]public Color BackgroundColor { get; private set; }
+    
+    
+    // zoom 
+    [Export]public float MinZoom { get; private set; }
+    [Export]public float MaxZoom { get; private set; }
+    [Export]public float ZoomSpeed { get; private set; }
 
     public override void _EnterTree()
     {
